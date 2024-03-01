de251c5b777d14bdf649f06f3b368950da2f1863
LeonardoRoncelli <160123628+LeonardoRoncelli@users.noreply.github.com>
2024-03-01 13:08:50 +0100

    Add files via upload
    Esercizio 1

---
+++ Metodi1.java
@@ -0,0 +1,17 @@
+
+public class Main {
+    public static void main(String[] args) {
+        mostraMessaggio();
+        mostraMessaggio2();
+        mostraMessaggio3();
+    }
+    private static void mostraMessaggio(){
+        System.out.println("Ciao");
+    }
+    private static void mostraMessaggio2(){
+        System.out.println("Oggi piove");
+    }
+    private static void mostraMessaggio3(){
+        System.out.println("Oggi è giovedì");
+    }
+}
